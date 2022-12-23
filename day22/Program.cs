b684595edbac2c39d02661064035b3720af49524
theperiscope <1619363+theperiscope@users.noreply.github.com>
2022-12-23 17:37:52 -0600

    day22: part 2 rewrite using neighbor maps

--- day22/Program.cs
+++ day22/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.Text;
 
+namespace day22;
+
+/// <summary>
+/// Monkey Map
+/// </summary>
 internal class Program
 {
     private static void Main(string[] args)
@@ -13,22 +18,19 @@ internal class Program
         var instructions = map[map.Count - 1];
         map.RemoveAt(map.Count - 1); // remove instructions
         map.RemoveAt(map.Count - 1); // and blank line, leaving only map
-        var maxLineLength = map.Select(line => line.Length).Max();
-        var spaces = new string(' ', maxLineLength);
-        map = map.Select(line => string.Concat(line, spaces.AsSpan(0, maxLineLength - line.Length))).ToList(); // make column lengths same
 
-        var rowLengths = map.Select(line => line.Trim().Length).ToArray();
-        var rowStartX = map.Select((line, i) => line.TrimEnd().LastIndexOf(' ') + 1).ToArray();
-        var columnHeights = new int[map[0].Length];
-        var columnStartY = new int[map[0].Length];
-        for (var i = 0; i < map[0].Length; i++) {
-            var s = new string(map.Select(line => line[i]).ToArray());
-            columnHeights[i] = s.Trim().Length;
-            columnStartY[i] = s.TrimEnd().LastIndexOf(' ') + 1;
+        if (args[0] == "input-short.txt") {
+            Solve(map, Blocks, N, instructions, structureMapPart1Short);
+            Solve(map, Blocks, N, instructions, structureMapCubeShort);
+        } else {
+            Solve(map, BigBlocks, NN, instructions, structureMapPart1);
+            Solve(map, BigBlocks, NN, instructions, structureMapCube);
         }
+    }
 
-        (int x, int y) cursor = (map[0].IndexOf('.'), 0);
-        var dir = '>';
+    private static void Solve(List<string> map, Dictionary<string, Block> blockMap, int blockSize, string instructions, Dictionary<string, NeighborMap> topology)
+    {
+        var state = new State("A", (0, 0), Dir.R);
         var ip = 0;
         while (true) {
             var sb = new StringBuilder();
@@ -38,21 +40,12 @@ internal class Program
             }
             var n = int.Parse(sb.ToString());
 
-            switch (dir) {
-                case '>':
-                    cursor = MoveRight(map, cursor, n, rowLengths, rowStartX);
-                    break;
-                case '<':
-                    cursor = MoveLeft(map, cursor, n, rowLengths, rowStartX);
-                    break;
-                case '^':
-                    cursor = MoveUp(map, cursor, n, columnHeights, columnStartY);
-                    break;
-                case 'v':
-                    cursor = MoveDown(map, cursor, n, columnHeights, columnStartY);
-                    break;
-                default:
-                    break;
+            for (var i = 0; i < n; i++) {
+                var stateNext = Move(topology, state, blockSize);
+                var global = ToInputCoordinates(blockMap, stateNext);
+                if (map[global.y][global.x] == '.') {
+                    state = stateNext;
+                }
             }
 
             if (ip >= instructions.Length - 1)
@@ -60,80 +53,153 @@ internal class Program
 
             var instructionDirection = instructions[ip];
             ip++;
-            dir = Turn(dir, instructionDirection == 'L');
+
+            switch (instructionDirection) {
+                case 'L':
+                    state = state with
+                    {
+                        dir = state.dir switch
+                        {
+                            Dir.R => Dir.U,
+                            Dir.D => Dir.R,
+                            Dir.L => Dir.D,
+                            Dir.U => Dir.L,
+                            _ => throw new NotImplementedException(),
+                        }
+                    };
+                    break;
+                case 'R':
+                    state = state with
+                    {
+                        dir = state.dir switch
+                        {
+                            Dir.R => Dir.D,
+                            Dir.D => Dir.L,
+                            Dir.L => Dir.U,
+                            Dir.U => Dir.R,
+                            _ => throw new NotImplementedException(),
+                        }
+                    };
+                    break;
+            }
         }
 
-        Console.WriteLine(1000 * (cursor.y + 1) + (4 * (cursor.x + 1)) + (dir switch { '>' => 0, 'v' => 1, '<' => 2, '^' => 3, _ => throw new InvalidOperationException() }));
-
+        Console.WriteLine(1000 * (ToInputCoordinates(blockMap, state).y + 1) + 4 * (ToInputCoordinates(blockMap, state).x + 1) + state.dir);
     }
 
-    private static (int x, int y) MoveLeft(List<string> map, (int x, int y) cursor, int n, int[] rowLengths, int[] rowStartX)
+    private static State Move(Dictionary<string, NeighborMap> structureMap, State state, int blockSize)
     {
-        var currentRowLength = rowLengths[cursor.y];
-        var startX = rowStartX[cursor.y];
-        for (var i = 1; i <= n; i++) {
-            var next = startX + Mod(cursor.x - 1, currentRowLength);
-            if (map[cursor.y][next] == '#') {
-                break;
-            } else
-                cursor.x = next;
+        bool isOutsideCurrentBlock((int x, int y) p) => p.x < 0 || p.x >= blockSize || p.y < 0 || p.y >= blockSize;
+        var (startBlock, p, dir) = state;
+        var endBlock = startBlock;
+        p = dir switch
+        {
+            Dir.R => p with { x = p.x + 1 },
+            Dir.D => p with { y = p.y + 1 },
+            Dir.L => p with { x = p.x - 1 },
+            Dir.U => p with { y = p.y - 1 },
+            _ => throw new InvalidOperationException()
+        };
+
+        if (isOutsideCurrentBlock(p)) {
+            var mapping = structureMap[startBlock];
+            var directionalNeighbor = dir switch
+            {
+                Dir.R => mapping.right,
+                Dir.D => mapping.down,
+                Dir.L => mapping.left,
+                Dir.U => mapping.up,
+                _ => throw new InvalidOperationException(),
+            };
+
+            endBlock = directionalNeighbor.name;
+
+            var rotate = directionalNeighbor.wrapRightRotations;
+
+            p = ((p.x + blockSize) % blockSize, (p.y + blockSize) % blockSize);
+
+            for (var i = 0; i < rotate; i++) {
+                p = p with { y = p.x, x = blockSize - p.y - 1 };
+                dir = dir switch
+                {
+                    Dir.R => Dir.D,
+                    Dir.D => Dir.L,
+                    Dir.L => Dir.U,
+                    Dir.U => Dir.R,
+                    _ => throw new NotImplementedException(),
+                };
+            }
         }
-        return cursor;
+
+        return new State(endBlock, p, dir);
     }
 
-    private static (int x, int y) MoveRight(List<string> map, (int x, int y) cursor, int n, int[] rowLengths, int[] rowStartX)
-    {
-        var currentRowLength = rowLengths[cursor.y];
-        var startX = rowStartX[cursor.y];
-        for (var i = 1; i <= n; i++) {
-            var next = startX + Mod(cursor.x + 1, currentRowLength);
-            if (map[cursor.y][next] == '#') {
-                break;
-            } else
-                cursor.x = next;
-        }
-        return cursor;
-    }
+    // block definition that matches input-short.txt
+    public const int N = 4;
+    public static Block A = new("A", (N * 2, 0));
+    public static Block B = new("B", (0, N));
+    public static Block C = new("C", (N, N));
+    public static Block D = new("D", (N * 2, N));
+    public static Block E = new("E", (N * 2, N * 2));
+    public static Block F = new("F", (N * 3, N * 2));
+    public static Dictionary<string, Block> Blocks = new() { { "A", A }, { "B", B }, { "C", C }, { "D", D }, { "E", E }, { "F", F } };
 
-    private static (int x, int y) MoveUp(List<string> map, (int x, int y) cursor, int n, int[] columnHeights, int[] columnStartY)
-    {
-        var currentColumnLength = columnHeights[cursor.x];
-        var startY = columnStartY[cursor.x];
-        for (var i = 1; i <= n; i++) {
-            var next = startY + Mod(cursor.y - 1, currentColumnLength);
-            if (map[next][cursor.x] == '#') {
-                break;
-            } else
-                cursor.y = next;
-        }
-        return cursor;
-    }
+    // block definition that matches input.txt
+    public const int NN = 50;
+    public static Block AA = new("A", (NN, 0));
+    public static Block BB = new("B", (NN * 2, 0));
+    public static Block CC = new("C", (NN, NN));
+    public static Block DD = new("D", (0, NN * 2));
+    public static Block EE = new("E", (NN, NN * 2));
+    public static Block FF = new("F", (0, NN * 3));
+    public static Dictionary<string, Block> BigBlocks = new() { { "A", AA }, { "B", BB }, { "C", CC }, { "D", DD }, { "E", EE }, { "F", FF } };
 
-    private static (int x, int y) MoveDown(List<string> map, (int x, int y) cursor, int n, int[] columnHeights, int[] columnStartY)
-    {
-        var currentColumnLength = columnHeights[cursor.x];
-        var startY = columnStartY[cursor.x];
-        for (var i = 1; i <= n; i++) {
-            var next = startY + Mod(cursor.y + 1, currentColumnLength);
-            if (map[next][cursor.x] == '#') {
-                break;
-            } else
-                cursor.y = next;
-        }
-        return cursor;
-    }
+    private static (int x, int y) ToInputCoordinates(Dictionary<string, Block> blocks, State state) => (blocks[state.block].from.x + state.cursor.x, blocks[state.block].from.y + state.cursor.y);
 
-    private static char Turn(char currentDirection, bool isLeft) => currentDirection switch
+    private static Dictionary<string, NeighborMap> structureMapPart1Short = new()
     {
-        '>' => isLeft ? '^' : 'v',
-        '<' => isLeft ? 'v' : '^',
-        '^' => isLeft ? '<' : '>',
-        'v' => isLeft ? '>' : '<',
-        _ => throw new InvalidOperationException(),
+        { "A", new(new("A", 0), new("C", 0), new("A", 0), new("E", 0)) },
+        { "B", new(new("C", 0), new("B", 0), new("D", 0), new("B", 0)) },
+        { "C", new(new("D", 0), new("C", 0), new("B", 0), new("C", 0)) },
+        { "D", new(new("B", 0), new("E", 0), new("C", 0), new("A", 0)) },
+        { "E", new(new("F", 0), new("A", 0), new("F", 0), new("D", 0)) },
+        { "F", new(new("E", 0), new("F", 0), new("E", 0), new("F", 0)) }
     };
 
-    /// <summary>
-    /// Modulus that works with negative numbers
-    /// </summary>
-    public static int Mod(int a, int b) => a % b < 0 ? a % b + b : a % b;
+    private static Dictionary<string, NeighborMap> structureMapPart1 = new()
+    {
+        { "A", new(new("B", 0), new("C", 0), new("B", 0), new("E", 0)) },
+        { "B", new(new("A", 0), new("B", 0), new("A", 0), new("B", 0)) },
+        { "C", new(new("C", 0), new("E", 0), new("C", 0), new("A", 0)) },
+        { "D", new(new("E", 0), new("F", 0), new("E", 0), new("F", 0)) },
+        { "E", new(new("D", 0), new("A", 0), new("D", 0), new("C", 0)) },
+        { "F", new(new("F", 0), new("D", 0), new("F", 0), new("D", 0)) }
+    };
+
+    private static Dictionary<string, NeighborMap> structureMapCubeShort = new()
+    {
+        { "A", new(new("F", 2), new("D", 0), new("C", 3), new("B", 2)) },
+        { "B", new(new("C", 0), new("E", 2), new("F", 1), new("A", 2)) },
+        { "C", new(new("D", 0), new("E", 3), new("B", 0), new("A", 1)) },
+        { "D", new(new("F", 1), new("E", 0), new("C", 0), new("A", 0)) },
+        { "E", new(new("F", 0), new("B", 2), new("C", 1), new("D", 0)) },
+        { "F", new(new("A", 2), new("B", 3), new("E", 0), new("D", 3)) }
+    };
+
+    private static Dictionary<string, NeighborMap> structureMapCube = new()
+    {
+        { "A", new(new("B", 0), new("C", 0), new("D", 2), new("F", 1)) },
+        { "B", new(new("E", 2), new("C", 1), new("A", 0), new("F", 0)) },
+        { "C", new(new("B", 3), new("E", 0), new("D", 3), new("A", 0)) },
+        { "D", new(new("E", 0), new("F", 0), new("A", 2), new("C", 1)) },
+        { "E", new(new("B", 2), new("F", 1), new("D", 0), new("C", 0)) },
+        { "F", new(new("E", 3), new("B", 0), new("A", 3), new("D", 0)) }
+    };
 }
+
+internal enum Dir { R = 0, D = 1, L = 2, U = 3 }
+internal record Block(string name, (int x, int y) from);
+internal record State(string block, (int x, int y) cursor, Dir dir);
+
+internal record NeighborMap(Neighbor right, Neighbor down, Neighbor left, Neighbor up);
+internal record Neighbor(string name, int wrapRightRotations);

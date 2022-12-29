39416f15c384b437d1e7fd33e37e9cde25a492fe
theperiscope <1619363+theperiscope@users.noreply.github.com>
2022-12-29 12:08:00 -0600

    day12: ~20x faster by implementing a reverse path scan (from end to all As); add performance timing

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,19 +1,40 @@
-﻿namespace day12;
+﻿using shared;
+
+namespace day12;
 
 /// <summary>
 /// Hill Climbing Algorithm
 /// </summary>
 internal class Program
 {
-    private static void Main(string[] args)
-    {
+    private static void Main(string[] args) {
         if (args.Length == 0) {
             Console.WriteLine("Usage: {0} <file>", Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]));
             return;
         }
 
-        var data = File.ReadAllLines(args[0]);
+        var (parseResults, parseTimings) = Perf.BenchmarkTime(() => Parse(args[0]));
+        var (grid, start, end, allAs) = parseResults[0];
+        Console.WriteLine($"Parsing: {grid.GetLength(0)}x{grid.GetLength(1)} grid in {parseTimings[0]:F2}ms");
 
+        var (part1Results, part1Timings) = Perf.BenchmarkTime(() => BFS(grid, start)(end).Count - 1);
+        Console.WriteLine($"Part 1 : {part1Results[0]} steps in {part1Timings[0]} ms");
+
+        var (part2Results, part2Timings) = Perf.BenchmarkTime(() => {
+            // instead of exploring N paths from all As to the end we do the reverse -
+            // explore 1 paths from the end and then scan for minimum A
+            var reversedPath = BFSReverse(grid, end);
+            var minSteps = allAs.Select(startA => reversedPath(startA).Count - 1).Where(x => x != -1).Min();
+            return minSteps;
+        });
+
+        Console.WriteLine($"Part 2 : {part2Results[0]} steps in {part2Timings[0]:F2}ms");
+        Console.WriteLine();
+        Console.WriteLine($"Total  : {parseTimings[0] + part1Timings[0] + part2Timings[0]:F2}ms");
+    }
+
+    private static (int[,] grid, Point start, Point end, List<Point> allAs) Parse(string fileName) {
+        var data = File.ReadAllLines(fileName);
         Point start = new(0, 0), end = new(0, 0);
         var allAs = new List<Point>(); // needed for part 2
         var grid = new int[data.Length + 2, data[0].Length + 2]; // borders (0s) to make getting neighbors more readable
@@ -26,22 +47,17 @@ internal class Program
                 if (data[i][j] == 'a')
                     allAs.Add(new Point(i, j));
 
-                grid[i + 1, j + 1] = data[i][j] switch { 'S' => '`', 'E' => '{', _ => data[i][j] };
+                grid[i + 1, j + 1] = data[i][j] switch { 'S' => '`', 'E' => '{', _ => data[i][j] }; // re-map start and end to character before/after in ASCII order
             }
         }
-
-        Console.WriteLine($"Part 1: {BFS(grid, start, end)(end).Count - 1} steps");
-
-        var minSteps = allAs.Select(startA => BFS(grid, startA, end)(end).Count - 1).Where(x => x != -1).Min();
-        Console.WriteLine($"Part 2: {minSteps} steps");
+        return (grid, start, end, allAs);
     }
 
     private static readonly List<Point> neighborCandidates = new() { new(-1, 0), new(1, 0), new(0, -1), new(0, 1) };
     private static readonly Point offset = new(1, 1);
 
-    private static Func<Point, IList<Point>> BFS(int[,] grid, Point start, Point end)
-    {
-        var previous = new Dictionary<Point, Point>();
+    private static Func<Point, IList<Point>> BFS(int[,] grid, Point start) {
+        var parents = new Dictionary<Point, Point>();
         var queue = new Queue<Point>();
 
         queue.Enqueue(start);
@@ -51,42 +67,57 @@ internal class Program
             var neighbors = neighborCandidates.Select(x => x + adjustedPoint).Where(x => grid[x.Row, x.Col] != 0).ToList();
 
             foreach (var neighbor in neighbors) {
-                if (previous.ContainsKey(neighbor - offset) ||
-                    grid[neighbor.Row, neighbor.Col] - grid[adjustedPoint.Row, adjustedPoint.Col] > 1)
+                if (parents.ContainsKey(neighbor - offset) ||
+                    grid[neighbor.Row, neighbor.Col] - grid[adjustedPoint.Row, adjustedPoint.Col] > 1) // same/ascending order
                     continue;
 
-                previous[neighbor - offset] = point;
+                parents[neighbor - offset] = point;
                 queue.Enqueue(neighbor - offset);
             }
         }
 
-        IList<Point> shortestPath(Point end)
-        {
-            var path = new List<Point>();
-            var current = end;
+        return end => GetShortestPath(parents, start, end);
+    }
 
-            while (!current.Equals(start)) {
-                path.Add(current);
+    private static Func<Point, IList<Point>> BFSReverse(int[,] grid, Point start) {
+        var parents = new Dictionary<Point, Point>();
+        var queue = new Queue<Point>();
 
-                if (!previous.ContainsKey(current)) // bad path
-                    return new List<Point>();
+        queue.Enqueue(start);
+        while (queue.Count > 0) {
+            var point = queue.Dequeue();
+            var adjustedPoint = point + offset;
+            var neighbors = neighborCandidates.Select(x => x + adjustedPoint).Where(x => grid[x.Row, x.Col] != 0).ToList();
 
-                current = previous[current];
+            foreach (var neighbor in neighbors) {
+                if (parents.ContainsKey(neighbor - offset)) continue;
+                if (!(grid[neighbor.Row, neighbor.Col] - grid[adjustedPoint.Row, adjustedPoint.Col] >= -1)) // same/descending order
+                    continue;
+
+                parents[neighbor - offset] = point;
+                queue.Enqueue(neighbor - offset);
             }
-
-            path.Add(start);
-            path.Reverse();
-            return path;
         }
 
-        return shortestPath;
+        return end => GetShortestPath(parents, start, end);
+    }
+
+    private static IList<Point> GetShortestPath(Dictionary<Point, Point> parents, Point start, Point end) {
+        var (path, current) = (new List<Point>(), end); // we'll be building path in reverse (from the end)
+        while (!current.Equals(start)) {
+            path.Add(current);
+            if (!parents.ContainsKey(current)) return new List<Point>(); // bad path
+            current = parents[current];
+        }
+        path.Add(start);
+        path.Reverse();
+        return path;
     }
 }
 
 internal record Point
 {
-    public Point(int row, int col)
-    {
+    public Point(int row, int col) {
         Row = row;
         Col = col;
     }

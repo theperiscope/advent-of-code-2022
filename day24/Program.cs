cb03b1ef292b380acc343d108f43f15cab2480de
theperiscope <1619363+theperiscope@users.noreply.github.com>
2022-12-30 13:04:53 -0600

    day24: 50x faster  by switching to string-based HashSet for blizzard locations

--- day24/Program.cs
+++ day24/Program.cs
@@ -1,4 +1,5 @@
 ﻿using shared;
+using System.Diagnostics;
 
 namespace day24;
 
@@ -7,20 +8,22 @@ namespace day24;
 /// </summary>
 internal class Program
 {
-    private static void Main(string[] args)
-    {
+    private static void Main(string[] args) {
         if (args.Length != 1) {
             Console.WriteLine("Usage: {0} <file>", Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]));
             return;
         }
 
+        var sw = new Stopwatch(); sw.Start();
         var input = File.ReadLines(args[0]).ToList();
         var (mapW, mapH) = (input[0].Length, input.Count);
 
         var blizzardMaps = new List<HashSet<Blizzard>>(); // index is time
+        var blizzardLocations = new List<HashSet<string>>();  // string type makes it easy to compare values later
         var blizzards = GetBlizzards(input);
         do {
             blizzardMaps.Add(blizzards);
+            blizzardLocations.Add(blizzards.Select(b => $"{b.Location.X},{b.Location.Y}").ToHashSet());
             blizzards = blizzards.Select(b => b.Next(mapW, mapH)).ToHashSet();
             if (blizzardMaps.Any(b => b.SetEquals(blizzards)))
                 break;
@@ -30,26 +33,24 @@ internal class Program
         var end = new Point(mapW - 2, mapH - 1);
         var initialState = new State(0, start);
 
-        Part1(initialState, end, mapW, mapH, blizzardMaps);
-        Part2(initialState, start, end, mapW, mapH, blizzardMaps);
+        Part1(initialState, end, mapW, mapH, blizzardLocations);
+        Part2(initialState, start, end, mapW, mapH, blizzardLocations);
+        sw.Stop(); Console.WriteLine(sw.Elapsed.TotalMilliseconds);
     }
 
-    private static void Part1(State initialState, Point end, int mapW, int mapH, List<HashSet<Blizzard>> blizzardMaps)
-    {
-        var state = FindPath(initialState, end, mapW, mapH, blizzardMaps);
+    private static void Part1(State initialState, Point end, int mapW, int mapH, List<HashSet<string>> blizzardLocations) {
+        var state = FindPath(initialState, end, mapW, mapH, blizzardLocations);
         Console.WriteLine($"Part 1: {state.time}");
     }
 
-    private static void Part2(State initialState, Point start, Point end, int mapW, int mapH, List<HashSet<Blizzard>> blizzardMaps)
-    {
-        var state = FindPath(initialState, end, mapW, mapH, blizzardMaps);
-        state = FindPath(state, start, mapW, mapH, blizzardMaps);
-        state = FindPath(state, end, mapW, mapH, blizzardMaps);
+    private static void Part2(State initialState, Point start, Point end, int mapW, int mapH, List<HashSet<string>> blizzardLocations) {
+        var state = FindPath(initialState, end, mapW, mapH, blizzardLocations);
+        state = FindPath(state, start, mapW, mapH, blizzardLocations);
+        state = FindPath(state, end, mapW, mapH, blizzardLocations);
         Console.WriteLine($"Part 2: {state.time}");
     }
 
-    private static State FindPath(State currentState, Point end, int mapWidth, int mapHeight, List<HashSet<Blizzard>> blizzardMaps)
-    {
+    private static State FindPath(State currentState, Point end, int mapWidth, int mapHeight, List<HashSet<string>> blizzardLocations) {
         var seen = new HashSet<State>();
         var q = new PriorityQueue<State, int>();
 
@@ -59,7 +60,7 @@ internal class Program
             if (state.location == end)
                 return state;
 
-            foreach (var option in Next(state, end, mapWidth, mapHeight, blizzardMaps)) {
+            foreach (var option in Next(state, end, mapWidth, mapHeight, blizzardLocations)) {
                 if (!seen.Contains(option)) {
                     seen.Add(option);
                     q.Enqueue(option, option.time + option.location.ManhattanDistanceTo(end));
@@ -70,21 +71,19 @@ internal class Program
         throw new InvalidOperationException();
     }
 
-    private static List<State> Next(State state, Point end, int mapWidth, int mapHeight, List<HashSet<Blizzard>> blizzardMaps)
-    {
+    private static List<State> Next(State state, Point end, int mapWidth, int mapHeight, List<HashSet<string>> blizzardLocations) {
         var options = new Point[] { new(0, 0), new(1, 0), new(0, 1), new(-1, 0), new(0, -1) };
         var nextStates = new List<State>();
         foreach (var option in options) {
             var proposedLocation = state.location + option;
-            var hasBlizzardInProposedLocation = blizzardMaps[(state.time + 1) % blizzardMaps.Count].Any(b => b.Location == proposedLocation);
+            var hasBlizzardInProposedLocation = blizzardLocations[(state.time + 1) % blizzardLocations.Count].Contains($"{proposedLocation.X},{proposedLocation.Y}");
             var proposedLocationWithinMapLimits =
                 proposedLocation == state.location ||
                 proposedLocation == end ||
                 (proposedLocation.X >= 1 && proposedLocation.X <= mapWidth - 2 && proposedLocation.Y >= 1 && proposedLocation.Y <= mapHeight - 2);
 
             if (!hasBlizzardInProposedLocation && proposedLocationWithinMapLimits) {
-                nextStates.Add(state with
-                {
+                nextStates.Add(state with {
                     time = state.time + 1,
                     location = proposedLocation
                 });
@@ -94,8 +93,7 @@ internal class Program
         return nextStates;
     }
 
-    private static HashSet<Blizzard> GetBlizzards(List<string> input)
-    {
+    private static HashSet<Blizzard> GetBlizzards(List<string> input) {
         var blizzards = new HashSet<Blizzard>();
         for (var y = 0; y < input.Count; y++) {
             var line = input[y];
@@ -103,8 +101,7 @@ internal class Program
                 if (!new[] { '>', 'v', '<', '^' }.Contains(line[x]))
                     continue;
 
-                var dir = line[x] switch
-                {
+                var dir = line[x] switch {
                     '>' => Dir.R,
                     'v' => Dir.D,
                     '<' => Dir.L,
@@ -128,16 +125,13 @@ internal record Blizzard
     public Point Location { get; }
     public Dir Direction { get; }
 
-    public Blizzard(Point location, Dir direction)
-    {
+    public Blizzard(Point location, Dir direction) {
         Location = location;
         Direction = direction;
     }
 
-    public Blizzard Next(int mapWidth, int mapHeight)
-    {
-        var offset = Direction switch
-        {
+    public Blizzard Next(int mapWidth, int mapHeight) {
+        var offset = Direction switch {
             Dir.R => new Point(1, 0),
             Dir.D => new Point(0, 1),
             Dir.L => new Point(-1, 0),

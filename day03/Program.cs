20264a4df787f1c34f6c7a66a4fb6598ce1f435d
theperiscope <1619363+theperiscope@users.noreply.github.com>
2023-01-02 17:14:27 -0600

    day03: rewrite and 5x faster using bit-based map/ops instead of HashSet intersections

--- day03/Program.cs
+++ day03/Program.cs
@@ -7,55 +7,74 @@ namespace day03
     /// </summary>
     internal class Program
     {
-        private static void Main(string[] args)
-        {
+        private static void Main(string[] args) {
             if (args.Length == 0) {
                 Console.WriteLine("Usage: {0} <file>", Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]));
                 return;
             }
 
-            Part1(args[0]);
-            Part2(args[0]);
+            var (parseResults, parseTimings) = Perf.BenchmarkTime(() => Parse(args[0]));
+            var input = parseResults[0];
+            Console.WriteLine($"Parsing: {input.Length} stacks in {parseTimings[0]:F2}ms");
+
+            var (part1Results, part1Timings) = Perf.BenchmarkTime(() => Part1(input));
+            var part1 = part1Results[0];
+            Console.WriteLine($"Part 1 : {part1} in {part1Timings[0]:F2}ms");
+
+            var (part2Results, part2Timings) = Perf.BenchmarkTime(() => Part2(input));
+            var part2 = part2Results[0];
+            Console.WriteLine($"Part 2 : {part2} in {part2Timings[0]:F2}ms");
+         
+            Console.WriteLine($"Total  : {parseTimings[0] + part1Timings[0] + part2Timings[0]:F2}ms");
         }
 
-        private static int ToPoints(char c)
-        {
-            return c switch {
-                >= 'A' and <= 'Z' => c - 'A' + 27,
-                >= 'a' and <= 'z' => c - 'a' + 1,
-                _ => throw new NotSupportedException()
-            };
+        private static string[] Parse(string fileName) => File.ReadAllLines(fileName);
+
+        private static int Part1(string[] lines) {
+            var sum = 0;
+            foreach (var s in lines) {
+                var (a, b) = (s[..(s.Length / 2)], s[(s.Length / 2)..]); // left and right side
+                var (A, B) = (0L, 0L);
+
+                // a and b have same length here by definition
+                for (var i = 0; i < a.Length; i++) {
+                    A |= 1L << (a[i] - 'A');
+                    B |= 1L << (b[i] - 'A');
+                }
+
+                // 0 bit - A, 25th bit - Z, 32nd bit - a, 57th bit = z
+                // priority: 0..25th bit (A..Z): i + 27
+                // priority: 0..25th bit (A..Z): i - 32 + 1
+                for (var i = 0; i < 64; i++) {
+                    // skip unused bits
+                    if (i < 'A' - 'A' || (i > 'Z' - 'A' && i < 'a' - 'A') || i > 'z' - 'A') continue;
+                    if (((A >> i) & 1) == 1 && ((B >> i) & 1) == 1) {
+                        sum += i >= 0 && i <= 25 ? i + 27 : i - 32 + 1;
+                    }
+                }
+            }
+            return sum;
         }
 
-        private static void Part1(string fileName)
-        {
-            var results = Utils.ReadAllLinesFrom(fileName)
-                .Select(line => {
-                    var left = line.Substring(0, line.Length / 2);
-                    var right = line.Substring(left.Length);
-                    return left.Length != right.Length ? throw new InvalidOperationException() : (left, right);
-                });
+        private static int Part2(string[] lines) {
+            var sum = 0;
+            foreach (var s in lines.Chunk(3)) {
+                var (a, b, c) = (s[0], s[1], s[2]);
+                var (A, B, C) = (0L, 0L, 0L);
 
-            var total1 = results.Select(x => x.left.ToHashSet()).ToList();
-            var total2 = results.Select(x => x.right.ToHashSet()).ToList();
+                // a, b, c here can have different lengths so multiple loops are needed
+                for (var i = 0; i < a.Length; i++) A |= 1L << (a[i] - 'A');
+                for (var i = 0; i < b.Length; i++) B |= 1L << (b[i] - 'A');
+                for (var i = 0; i < c.Length; i++) C |= 1L << (c[i] - 'A');
 
-            var points = total1.Select((x, i) => {
-                var (n1, n2) = (total1[i], total2[i]);
-                return ToPoints(n1.Intersect(n2).First());
-            }).Sum();
-
-            Console.WriteLine(points);
+                for (var i = 0; i < 64; i++) {
+                    if (i < 'A' - 'A' || (i > 'Z' - 'A' && i < 'a' - 'A') || i > 'z' - 'A') continue;
+                    if (((A >> i) & 1) == 1 && ((B >> i) & 1) == 1 && ((C >> i) & 1) == 1) {
+                        sum += i >= 0 && i <= 25 ? i + 27 : i - 32 + 1;
+                    }
+                }
+            }
+            return sum;
         }
-
-        private static void Part2(string fileName)
-        {
-            var groups = Utils.ReadAllLinesFrom(fileName)
-                .Chunk(3).Select(chunk => (chunk[0].ToHashSet(), chunk[1].ToHashSet(), chunk[2].ToHashSet()));
-
-            var points = groups.Select(x => ToPoints(x.Item1.Intersect(x.Item2).Intersect(x.Item3).First())).Sum();
-
-            Console.WriteLine(points);
-        }
-
     }
 }

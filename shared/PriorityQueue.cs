baaf25fb41d938e318f2f67f3b137719877f013c
theperiscope <1619363+theperiscope@users.noreply.github.com>
2023-01-05 19:54:56 -0600

    make PriorityQueue more readable

--- shared/PriorityQueue.cs
+++ shared/PriorityQueue.cs
@@ -21,33 +21,33 @@
     /// Heap height is H = log2 N
     /// Leaf nodes are at height H or H-1
     /// Heap of height H has between h+1 and 2^(h+1)+1 nodes
-    /// Arr[(i-1)/2]	Returns the parent node
-    /// Arr[(2 * i) + 1] Returns the left child node
-    /// Arr[(2 * i) + 2] Returns the right child node
+    /// Arr[(i-1)/2]	Returns the parentIndex node
+    /// Arr[(2 * i) + 1] Returns the left childIndex node
+    /// Arr[(2 * i) + 2] Returns the right childIndex node
     /// leafs start from floor(n/2) index
     /// </summary>
     /// <typeparam name="T">element type, must implement <see cref="IComparable"/></typeparam>
     public class PriorityQueue<T> where T : IComparable<T>
     {
-        public PriorityQueue(bool isMinPriorityQueue = false)
-        {
+        public PriorityQueue(bool isMinPriorityQueue = false, IEnumerable<T>? initialInput = null) {
             this.isMinPriorityQueue = isMinPriorityQueue;
             queue = new List<T>();
+            if (initialInput != null) {
+                foreach (T item in initialInput) Enqueue(item);
+            }
         }
 
         public List<T> queue;
         private readonly bool isMinPriorityQueue;
 
-        public void Enqueue(T item)
-        {
+        public void Enqueue(T item) {
             queue.Add(item);
             if (isMinPriorityQueue)
                 HeapifyUpMin(queue);
             else
                 HeapifyUpMax(queue);
         }
-        public T Dequeue()
-        {
+        public T Dequeue() {
             if (queue.Count == 0)
                 throw new Exception("Queue is empty");
 
@@ -62,23 +62,30 @@
 
             return returnVal;
         }
-        public T Peek() { return queue[0]; }
-        public int Count() { return queue.Count; }
 
-        public int Height()
-        {
-            return Height(Count());
+        public IEnumerable<T> DequeueAll() {
+            while (!IsEmpty()) yield return Dequeue();
         }
 
-        public int Height(int n)
-        {
-            return (int)Math.Floor(Math.Log2(n));
-        }
+        public static int GetLeftChildIndex(int parentIndex) => 2 * parentIndex + 1;
+        public static int GetRightChildIndex(int parentIndex) => 2 * parentIndex + 2;
+        public static int GetParentIndex(int childIndex) => (childIndex - 1) / 2;
+        public static bool HasLeftChild(IList<T> arr, int parentIndex) => GetLeftChildIndex(parentIndex) <= arr.Count - 1;
+        public static bool HasRightChild(IList<T> arr, int parentIndex) => GetRightChildIndex(parentIndex) <= arr.Count - 1;
+        public static bool HasParent(int childIndex) => GetParentIndex(childIndex) >= 0;
 
-        public List<T> Leafs()
-        {
-            // last leaf is Nth element (index N-1). it's parent is N/2-nd element (index (N-1)/2).
-            // there is no element such that its parent is after N/2-nd elemnent (index (N-1)/2)
+        public T Peek() => queue[0];
+        public int Count() => queue.Count;
+        public int Height() => Height(Count());
+        public int Height(int n) => (int)Math.Floor(Math.Log2(n));
+        public bool IsEmpty() => queue.Count == 0;
+        public T? GetParent(int childIndex) => HasParent(childIndex) ? queue[GetParentIndex(childIndex)] : default;
+        public T? GetLeftChild(int parentIndex) => HasLeftChild(queue, parentIndex) ? queue[GetLeftChildIndex(parentIndex)] : default;
+        public T? GetRightChild(int parentIndex) => HasRightChild(queue, parentIndex) ? queue[GetRightChildIndex(parentIndex)] : default;
+
+        public List<T> Leafs() {
+            // last leaf is Nth element (index N-1). it's parentIndex is N/2-nd element (index (N-1)/2).
+            // there is no element such that its parentIndex is after N/2-nd elemnent (index (N-1)/2)
             // so, N/2+1 element and onwards (n/2 index and onwards) are all leafs
             var n = Count();
             var leafs = new List<T>();
@@ -92,12 +99,11 @@
         /// <summary>
         /// called for heap deletion
         /// </summary>
-        private static void HeapifyDownMin<T>(IList<T> arr, int i) where T : IComparable<T>
-        {
-            var (left, right, N) = (2 * i + 1, 2 * i + 2, arr.Count - 1);
-            var smallest = left <= N && arr[left].CompareTo(arr[i]) < 0 ? left : i;
+        private static void HeapifyDownMin(IList<T> arr, int i) {
+            var (left, right) = (GetLeftChildIndex(i), GetRightChildIndex(i));
+            var smallest = HasLeftChild(arr, i) && arr[left].CompareTo(arr[i]) < 0 ? left : i;
 
-            if (right <= N && arr[right].CompareTo(arr[smallest]) < 0)
+            if (HasRightChild(arr, i) && arr[right].CompareTo(arr[smallest]) < 0)
                 smallest = right;
             if (smallest != i) {
                 (arr[i], arr[smallest]) = (arr[smallest], arr[i]);
@@ -108,12 +114,11 @@
         /// <summary>
         /// called for heap deletion
         /// </summary>
-        private static void HeapifyDownMax<T>(IList<T> arr, int i) where T : IComparable<T>
-        {
-            var (left, right, N) = (2 * i + 1, 2 * i + 2, arr.Count - 1);
-            var largest = left <= N && arr[left].CompareTo(arr[i]) > 0 ? left : i;
+        private static void HeapifyDownMax(IList<T> arr, int i) {
+            var (left, right) = (GetLeftChildIndex(i), GetRightChildIndex(i));
+            var largest = HasLeftChild(arr, i) && arr[left].CompareTo(arr[i]) > 0 ? left : i;
 
-            if (right <= N && arr[right].CompareTo(arr[largest]) > 0)
+            if (HasRightChild(arr, i) && arr[right].CompareTo(arr[largest]) > 0)
                 largest = right;
             if (largest != i) {
                 (arr[i], arr[largest]) = (arr[largest], arr[i]);
@@ -124,24 +129,27 @@
         /// <summary>
         /// called for heap insertion
         /// </summary>
-        private static void HeapifyUpMax<T>(IList<T> arr) where T : IComparable<T>
-        {
+        private static void HeapifyUpMax(IList<T> arr) {
             var i = arr.Count - 1;
-            while (i >= 0 && arr[(i - 1) / 2].CompareTo(arr[i]) < 0) {
-                (arr[i], arr[(i - 1) / 2]) = (arr[(i - 1) / 2], arr[i]);
-                i = (i - 1) / 2;
+            while (HasParent(i) && arr[GetParentIndex(i)].CompareTo(arr[i]) < 0) {
+                (arr[i], arr[GetParentIndex(i)]) = (arr[GetParentIndex(i)], arr[i]);
+                i = GetParentIndex(i);
             }
         }
         /// <summary>
         /// called for heap insertion
         /// </summary>
-        private static void HeapifyUpMin<T>(IList<T> arr) where T : IComparable<T>
-        {
+        private static void HeapifyUpMin(IList<T> arr) {
             var i = arr.Count - 1;
-            while (i >= 0 && arr[(i - 1) / 2].CompareTo(arr[i]) > 0) {
-                (arr[i], arr[(i - 1) / 2]) = (arr[(i - 1) / 2], arr[i]);
-                i = (i - 1) / 2;
+            while (HasParent(i) && arr[GetParentIndex(i)].CompareTo(arr[i]) > 0) {
+                (arr[i], arr[GetParentIndex(i)]) = (arr[GetParentIndex(i)], arr[i]);
+                i = GetParentIndex(i);
             }
         }
+
+        /// <summary>
+        /// Sort input using Heap Sort
+        /// </summary>
+        public static IEnumerable<T> Sort(IEnumerable<T> input, bool isAscending) => new PriorityQueue<T>(isAscending, input).DequeueAll();
     }
 }
